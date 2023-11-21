f3a76c61e0f472ec65b19a887e0e41b5caa1ad40
s0011x <suha.alrajhi01@gmail.com>
2023-11-21 19:21:43 +0300

    Splash

---
+++ MC2TEAM2/UserName.swift
@@ -0,0 +1,18 @@
+//
+//  UserName.swift
+//  MC2TEAM2
+//
+//  Created by suha alrajhi on 05/05/1445 AH.
+//
+
+import SwiftUI
+
+struct UserName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserName()
+}
---
+++ MC2TEAM2/splash.swift
@@ -0,0 +1,18 @@
+//
+//  splash.swift
+//  MC2TEAM2
+//
+//  Created by suha alrajhi on 05/05/1445 AH.
+//
+
+import SwiftUI
+
+struct splash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    splash()
+}
