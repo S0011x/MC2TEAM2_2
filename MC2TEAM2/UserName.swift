05f6c078e2dfea586ab65663428a2849ee74c6d1
s0011x <suha.alrajhi01@gmail.com>
2023-11-21 19:27:17 +0300

    splash

--- MC2TEAM2/UserName.swift
+++ MC2TEAM2/UserName.swift
@@ -5,13 +5,95 @@
 //  Created by suha alrajhi on 05/05/1445 AH.
 //
 
+
 import SwiftUI
 
 struct UserName: View {
+    @State private var nameText = ""
+    @State private var isButtonTapped = false
+    //send name to user goal
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //NavigationStack{
+        ZStack(alignment: .bottom){
+
+            
+            VStack{
+                Image("")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .frame(width: 350,height: 350)
+                    .padding(.bottom,315)
+            }
+      
+            
+         
+            VStack{
+                Text(" شاركنا باسمك !")
+                    .font(.system(size: 16))
+                TextField("ادخل اسمك هنا", text: $nameText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 330,height:48)
+                    .padding(.bottom,230)
+            }
+            
+            
+            
+            VStack{
+ Text(" أهلا! حاب اتعرف عليك ")
+                
+                    .font(.system(size: 16))
+                    .bold()
+
+                    .padding(.bottom,620)
+                    .padding(.leading,10)
+                    
+            }
+        VStack{
+            Spacer()
+            Image("bottomEffect")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .frame(width: 1000)
+                .padding(.trailing,40)
+                .padding(.top,500)
+            
+        }
+        VStack {
+            
+   // NavigationLink("التالي",destination:ContentView())
+ //   NavigationLink(destination: ContentView()) {
+                
+                Button("التالي", action:{
+                    isButtonTapped = true
+                })
+              
+                .font(.system(size: 20))
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 330 ,height: 48)
+                
+                .cornerRadius(24)
+                .padding(.bottom,50)
+                
+         //   }  //navigation
+    // .fullScreenCover(isPresented: $isButtonTapped, content: {
+            //ContentView(name:name )
+                           // })
+        }
+      //  .navigationBarTitle("المعلومات المالية")
+
+        
+    
+}
+    .padding()
+         //navStack
+
+      //  .navigationBarTitle(Text("Login"))
+        
     }
 }
+
+
 
 #Preview {
     UserName()
--- MC2TEAM2/splash.swift
+++ MC2TEAM2/splash.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct splash: View {
+    @State var animate: Bool = false
+    @State var showSplash: Bool = true
+    @State private var showIntro: Bool = false // Set initial value to false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background
+            Color(hex: 0x4AD294) // Change the background color here
+
+            // Logo
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400, height: 300)
+                .padding(.leading, 15.0)
+                .scaleEffect(animate ? 11 : 1)
+                .animation(Animation.easeIn(duration: 1))
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        animate.toggle()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            showIntro.toggle() // Show the intro after the splash screen animation
+                        }
+                    }
+                }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .opacity(showSplash ? 1 : 0) // Change opacity value
+        .animation(.default)
+        .overlay(
+            Group {
+                if showIntro {
+                    ContentView()
+                }
+            }
+        )
     }
 }
+
+
+
+extension Color {
+    init(hex: UInt) {
+        let red = Double((hex >> 16) & 0xFF) / 255.0
+        let green = Double((hex >> 8) & 0xFF) / 255.0
+        let blue = Double(hex & 0xFF) / 255.0
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
 
 #Preview {
     splash()
 }
+
+
