5f934b1e20a260fe341f657fd02cb327c4f651bf
Hams Alzahrani <hamsmohsen692@gmail.com>
2023-11-26 10:16:59 +0300

    Adding new pages+ editing the UI

---
+++ MC2TEAM2/FirstLevel.swift
@@ -0,0 +1,18 @@
+//
+//  FirstLevel.swift
+//  MC2TEAM2
+//
+//  Created by Hams Alzahrani on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct FirstLevel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstLevel()
+}
---
+++ MC2TEAM2/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  MC2TEAM2
+//
+//  Created by Hams Alzahrani on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
