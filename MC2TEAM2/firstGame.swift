b7afdead508914d70c2a354546c9604542229c2d
s0011x <suha.alrajhi01@gmail.com>
2023-11-26 09:39:51 +0300

    the first 5 screens

---
+++ MC2TEAM2/firstGame.swift
@@ -0,0 +1,18 @@
+//
+//  firstGame.swift
+//  MC2TEAM2
+//
+//  Created by suha alrajhi on 11/05/1445 AH.
+//
+
+import SwiftUI
+
+struct firstGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    firstGame()
+}
---
+++ MC2TEAM2/gameScreen.swift
@@ -0,0 +1,18 @@
+//
+//  gameScreen.swift
+//  MC2TEAM2
+//
+//  Created by suha alrajhi on 11/05/1445 AH.
+//
+
+import SwiftUI
+
+struct gameScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    gameScreen()
+}
