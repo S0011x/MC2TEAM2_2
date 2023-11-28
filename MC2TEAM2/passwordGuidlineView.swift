8488e9094a8c8d24129279fcdd0042ccd94e5217
Shahad842 <shahad_9215@hotmail.com>
2023-11-28 22:11:32 +0300

    new update 1

---
+++ MC2TEAM2/passwordGuidlineView.swift
@@ -0,0 +1,18 @@
+//
+//  passwordGuidlineView.swift
+//  MC2TEAM2
+//
+//  Created by Shahad Alhothali on 14/05/1445 AH.
+//
+
+import SwiftUI
+
+struct passwordGuidlineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    passwordGuidlineView()
+}
