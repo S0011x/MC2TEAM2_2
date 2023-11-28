99784cb0ab998613bbff4fc107be3c6bcc0e3941
Shahad842 <shahad_9215@hotmail.com>
2023-11-28 22:19:36 +0300

    new update3

--- MC2TEAM2/passwordGiudlineView.swift
+++ MC2TEAM2/passwordGiudlineView.swift
@@ -7,12 +7,49 @@
 
 import SwiftUI
 
-struct passwordGiudlineView: View {
+struct passwordGuidlineView: View {
+    let texts = ["هناك معايير يجب اخذها بعين الإعتبار عند إنشاء كلمة  مرور قوية .لاتكتب معلوماتك الشخصية مثل اسمك وعمرك .يجب أن تحتوي  على الأقل حرف كبير حرف صغير ورمز .لاتحتوي على ارقام متسلسلة .يجب أن يكون سهل عليك تذكره صعب على الآخرين تذكرة","sconed tex"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+        VStack {
+                Text(" معايير كلمة المرور")
+                    .font(
+                        Font.custom("Inter", size: 32)
+                            .weight(.semibold))
+                    .padding(.top, 40)
+                    .padding(.bottom, -50)
+                TabView {
+                    ForEach(texts, id: \.self) { text in
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 330, height: 600)
+                            .background(Color(red: 0.67, green: 0.87, blue: 0.78))
+                            .cornerRadius(10)
+                            .shadow(color: .black.opacity(0.25), radius: 2, x: 1, y: 2)
+                            .overlay(
+                                Text(text)
+                                    .foregroundColor(.black)
+                                    .font(.title)
+                                    .multilineTextAlignment(.center)
+                                    .padding(/*@START_MENU_TOKEN@*/EdgeInsets()/*@END_MENU_TOKEN@*/)
+                                    .padding()
+                                
+                            )
+                            .tag(text)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+        }
     }
 }
-
 #Preview {
-    passwordGiudlineView()
+
 }
+
